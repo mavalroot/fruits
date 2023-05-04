9744579a206c288243289d785f0c40dd0be0b2d7
María Valderrama Rodríguez <mavalroot@gmail.com>
2023-05-04 09:22:42 +0200

    basic config + file structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}'],
+  },
+  important: true,
+};

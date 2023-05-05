f5d1fd0bd4560769e42a18bc33ab1750472c1f8d
María Valderrama <mavalroot@gmail.com>
2023-05-05 17:48:49 +0200

    feat: navbar

---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MatIconModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StateModule } from './state/state.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+
+const EXPORT_IMPORT = [
+  NavbarComponent
+];
 @NgModule({
   declarations: [],
   imports: [
     HttpClientModule,
-    StateModule
-  ]
+    StateModule,
+    ...EXPORT_IMPORT
+  ],
+  exports: EXPORT_IMPORT
 })
 export class CoreModule { }
